e4ce0c488db85dd22ccf27acb542a4687ce25625
littleRaccoonInvestigator <37294485+littleRaccoonInvestigator@users.noreply.github.com>
2019-05-17 16:11:37 +0800

    Update save&loadlattice.py

--- save&loadlattice.py
+++ save&loadlattice.py
@@ -3,19 +3,22 @@ import pickle
 
 def save_lattice(lattice, sizeOfSample, filename):
     try:
-        f = open(str(filename) + ".dat", "wb")
-        pickle.dump(lattice,f)
-    except:
+        filepointer = open(str(filename) + ".dat", "wb")
+        pickle.dump(lattice,filepointer)
+        filepointer.close()
+    except Exception as inst:
         print("An error occurred when saving data")
-    finally:
-        f.close() 
+        print(inst.args)
+        return 
+   
         
 def load_lattice(sizeOfSample, filename):
     try:
-        f = open(str(filename) + ".dat", "rb")
-        lattice = pickle.load(f)
-    except:
+        filepointer = open("ground_state_24.dat", "rb")
+        lat = pickle.load(filepointer)
+        filepointer.close()
+    except Exception as inst:
         print("An error occurred when loading data")
-    finally:
-        f.close()
-    return lattice
+        print(inst.args)
+        return None
+    return lat
